59fd82f88f8f0b9784f3aa898546bd0dc189b22d
Coubick <samandr0405@gmail.com>
2025-08-13 16:59:57 +0300

    Initial commit

--- Storage/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.consumers"},{"l":"org.example.dataStorage"},{"l":"org.example.entities"},{"l":"org.example.services"}];updateSearchResults();
